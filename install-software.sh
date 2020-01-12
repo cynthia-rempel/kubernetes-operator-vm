ba54b9c738bca6bb0e48ed8d3129a83f242544be
cynthia-rempel <cynthia.m.rempel@gmail.com>
2020-01-11 19:44:21 -0800

    Update install-software.sh

--- install-software.sh
+++ install-software.sh
@@ -5,7 +5,7 @@ rpm --import https://download.docker.com/linux/centos/gpg
 
 dnf install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
 
-dnf install docker-ce
+dnf install docker-ce.x86_64
 
 systemctl disable firewalld
 systemctl enable docker
