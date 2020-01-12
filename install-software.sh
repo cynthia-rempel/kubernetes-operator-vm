ce6a1e48aa65d4e49f80abb5e1c678dc87082f66
cynthia-rempel <cynthia.m.rempel@gmail.com>
2020-01-11 19:31:52 -0800

    Update install-software.sh

--- install-software.sh
+++ install-software.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+cd /etc/yum.repos.d/
+wget https://download.docker.com/linux/centos/docker-ce.repo 
+rpm --import https://download.docker.com/linux/centos/gpg
 
 dnf install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
 
