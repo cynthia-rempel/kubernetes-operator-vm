14ac1ca6f2d424e0309434aa25a21ac708aedb6d
cynthia-rempel <cynthia.m.rempel@gmail.com>
2020-01-11 22:12:55 -0800

    Update install-software.sh

--- install-software.sh
+++ install-software.sh
@@ -22,7 +22,7 @@ repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
 
-yum -y install git kubeadm
+yum -y install git-core kubeadm
 
 swapoff -a
 
