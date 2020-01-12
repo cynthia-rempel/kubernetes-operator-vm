bb2e7d6fb7cb103e191fe746cde554ad18641d5f
cynthia-rempel <cynthia.m.rempel@gmail.com>
2020-01-11 20:41:30 -0800

    Update install-software.sh

--- install-software.sh
+++ install-software.sh
@@ -24,6 +24,8 @@ EOF
 
 yum -y install kubeadm
 
+swapoff -a
+
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
   && chmod +x minikube \
   && mv minikube /usr/local/bin \
