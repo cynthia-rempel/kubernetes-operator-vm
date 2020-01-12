190af6430362261cb3da1d0bc2f6a375c5daefeb
cynthia-rempel <cynthia.m.rempel@gmail.com>
2020-01-11 20:00:11 -0800

    Update install-software.sh

--- install-software.sh
+++ install-software.sh
@@ -12,6 +12,18 @@ systemctl enable docker
 systemctl start docker
 usermod -aG docker $USER
 
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+yum -y install kubeadm
+
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
   && chmod +x minikube \
   && mv minikube /usr/local/bin \
